599136e52504a5fd7b5fb5810c655fd932676bb7
Kirill Smirnov <kirill.smirnov@keystone.local>
2026-02-06 14:19:52 +0300

    build: add base dependencies and base directory structure, unable typescript checking for build command

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Html lang="ru" dir="ltr">
+    <div class="default">
+      <slot/>
+    </div>
+  </Html>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const x = "str"
+</script>
+
+<template>
+  <div class="base">
+    Ну привет)
+  </div>
+</template>
+
+<style scoped lang="scss">
+.base {
+  color: lightskyblue;
+}
+</style>
