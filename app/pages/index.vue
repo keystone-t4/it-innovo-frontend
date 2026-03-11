e4006525a2f0f2033f367f7bca1e6ad222db054f
Kirill Smirnov <kirill.smirnov@keystone.local>
2026-03-11 10:29:21 +0300

    style: finish about section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -25,7 +25,78 @@
       </div>
     </div>
 
+    <section class="about block">
+      <h1 class="about__title">
+        Когда бизнес перерастает
+        <span class="accent-text">ручное управление</span>
+      </h1>
+      <p class="about__description">
+        Бизнес растёт.<br>Объём поставок увеличивается.<br>Штат расширяется.
+        <br><br>И в какой-то момент:
+      </p>
+      <div class="about__cards">
+        <div class="about__card">
+          <p class="about__card-description">
+            смены работают в&nbsp;режиме ожидания
+          </p>
+          <svg class="about__card-icon" aria-hidden="true">
+            <use href="/sprite.svg#arrow" />
+          </svg>
+        </div>
+        <div class="about__card">
+          <p class="about__card-description">
+            появляются авралы и&nbsp;сверхурочные
+          </p>
+          <svg class="about__card-icon" aria-hidden="true">
+            <use href="/sprite.svg#arrow" />
+          </svg>
+        </div>
+        <div class="about__card">
+          <p class="about__card-description">
+            теряется прозрачность загрузки
+          </p>
+          <svg class="about__card-icon" aria-hidden="true">
+            <use href="/sprite.svg#arrow" />
+          </svg>
+        </div>
+        <div class="about__card">
+          <p class="about__card-description">
+            учёт становится фрагментарным
+          </p>
+          <svg class="about__card-icon" aria-hidden="true">
+            <use href="/sprite.svg#arrow" />
+          </svg>
+        </div>
+        <div class="about__card">
+          <p class="about__card-description">
+            решения принимаются «вручную»
+          </p>
+          <svg class="about__card-icon" aria-hidden="true">
+            <use href="/sprite.svg#arrow" />
+          </svg>
+        </div>
+        <div class="about__card-conclusion">
+          <p class="about__card-conclusion-text">
+            В результате бизнес начинает нести операционные потери.
+          </p>
+        </div>
+      </div>
 
+      <div class="about__footer">
+        <div class="about__footer-text">
+          <p class="about__description">
+            Именно на этом этапе требуется системное решение.
+          </p>
+          <h2 class="about__conclusion">
+            Инново анализирует процессы предприятия и выстраивает систему,
+            которая делает загрузку, смены и учёт прозрачными и управляемыми.
+          </h2>
+        </div>
+        <svg class="about__footer-icon" aria-hidden="true">
+          <use href="/sprite.svg#logo-transparent"></use>
+        </svg>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -34,6 +105,13 @@
   width: 100%;
   padding: 0 1.5rem;
 }
+
+section {
+  padding-top: 100px;
+  padding-bottom: 100px;
+}
+
+
 
 .hero {
   max-width: 1920px;
@@ -64,6 +142,78 @@
   &__buttons {
     display: flex;
     gap: 18px;
+  }
+}
+
+
+
+.about {
+  margin: 0 auto;
+
+  &__title {
+    max-width: 750px;
+    margin-bottom: 30px;
+  }
+
+  &__cards {
+    margin-top: 30px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 24px;
+    @media (max-width: 1024px) {
+      grid-template-columns: 1fr 1fr;
+    }
+    @media (max-width: 576px) {
+      grid-template-columns: 1fr;
+    }
+  }
+
+  &__card {
+    padding: 30px 40px;
+    background-color: var(--color-light-gray);
+    border-radius: 10px;
+    display: flex;
+    gap: 10px;
+    @media (max-width: 1024px) {
+      padding: 20px 30px;
+    }
+    @media (max-width: 576px) {
+      padding: 15px 25px;
+    }
+  }
+
+  &__card-icon {
+    margin-top: auto;
+    width: 34px;
+    height: 20px;
+  }
+
+  &__card-conclusion {
+    padding: 30px 40px;
+    border: 3px solid var(--color-accent);
+    border-radius: 10px;
+    @media (max-width: 1024px) {
+      padding: 20px 30px;
+    }
+    @media (max-width: 576px) {
+      padding: 15px 25px;
+    }
+  }
+
+  &__card-conclusion-text {
+    font-weight: 600;
+  }
+
+  &__footer {
+    margin-top: 70px;
+    display: flex;
+    gap: 20px;
+  }
+
+  &__footer-text {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
   }
 }
 </style>
